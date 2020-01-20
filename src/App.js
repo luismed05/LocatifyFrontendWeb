2442c2842dda22e122021238d9a71e312a739bb6
Luis Felipe <luis_medeiros96@yahoo.com.br>
2020-01-20 18:17:47 +0000

    commit start map

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Map
+          google={window.google}
+          zoom={8}
+          style={{flex:1}}
+          initialCenter={{lat: -15.786849,lng: -47.8723847}}
+        />
+    </>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDTeiTQvNwHj5fJbKex5hoWSVskZ7qF5HA'
+})(App);
